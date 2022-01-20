73c50c4fd34840b3422364724d1cc200e51a81de
Ana <s344046@oslomet.no>
2022-01-20 11:15:40 +0100

    Javascript1 Kodegjennomgang - All exercises

---
+++ src/main/java/data1700/gjennomgang/javascript1/Javascript1Application.java
@@ -0,0 +1,13 @@
+package data1700.gjennomgang.javascript1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javascript1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javascript1Application.class, args);
+    }
+
+}
---
+++ src/test/java/data1700/gjennomgang/javascript1/Javascript1ApplicationTests.java
@@ -0,0 +1,13 @@
+package data1700.gjennomgang.javascript1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javascript1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
